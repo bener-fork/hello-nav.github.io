d07914595efa62d299c6a7ccd779b8e94da7d02b
Bener <2388697970@qq.com>
2021-09-12 05:40:44 +0000

    CI

---
+++ precache-manifest.61d1a70ea186ad5530a4a182c7209583.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "022d1c0e728f7c986d339f3b5201e08f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89a98fd9579b5ab7e885",
+    "url": "/static/js/2.f0d0b683.chunk.js"
+  },
+  {
+    "revision": "218ea23999a0af6198fe",
+    "url": "/static/js/main.cec20be9.chunk.js"
+  },
+  {
+    "revision": "7afb10e8d62a811a23cb",
+    "url": "/static/js/runtime~main.46b21fbf.js"
+  },
+  {
+    "revision": "3f2d405be1bc8ee5acadd7fdf330ce55",
+    "url": "/static/media/angular.3f2d405b.svg"
+  },
+  {
+    "revision": "2100edafbd1cab6b3564be1fdcc7c119",
+    "url": "/static/media/bi.2100edaf.woff"
+  },
+  {
+    "revision": "5b504fca88a64d9e187a483a9c7af16a",
+    "url": "/static/media/bi.5b504fca.ttf"
+  },
+  {
+    "revision": "d63d573fd54e88d8ea20dae571f1bdde",
+    "url": "/static/media/goober.d63d573f.png"
+  },
+  {
+    "revision": "d41014a43be2ab589e539d41d93e406b",
+    "url": "/static/media/jenkins.d41014a4.png"
+  },
+  {
+    "revision": "89be4a45b064c97f8034e9e612caae8f",
+    "url": "/static/media/lerna.89be4a45.png"
+  },
+  {
+    "revision": "2d8ee815146390d567706f2c7b5c2916",
+    "url": "/static/media/nyantocat.2d8ee815.gif"
+  },
+  {
+    "revision": "b48b281ea9e16c6da6e5a9e9eb586d4f",
+    "url": "/static/media/tinypng.b48b281e.png"
+  }
+]);
